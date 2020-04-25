57fc9d41d96691148f753da6665e993e0e2f80ca
darwin christopher tantuco <dcrtantuco@gmail.com>
2020-04-26 00:33:28 +0800

    Minor style improvements on sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Layout from '../components/Layout'
 const IndexPage = (): JSX.Element => {
   return (
     <Layout>
-      <h1> Notes </h1>
+      <h2> Notes </h2>
       <p> Collection of notes I find useful in day to day work </p>
     </Layout>
   )
