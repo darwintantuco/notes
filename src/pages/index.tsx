02faba1005d1356b0a531fe6e8935303fee52ce6
darwin christopher tantuco <dcrtantuco@gmail.com>
2020-04-25 18:30:01 +0800

    Able to display notes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
-import SEO from '../components/SEO'
+import NoteLink from '../components/NoteLink'
 
-const Index = (): JSX.Element => (
-  <Layout>
-    <SEO />
-    <main>
-      <h1> Hello World. </h1>
-    </main>
-  </Layout>
-)
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}): JSX.Element => {
+  const Posts = edges.map((edge) => (
+    <NoteLink key={edge.node.id} post={edge.node} />
+  ))
 
-export default Index
+  return <Layout>{Posts}</Layout>
+}
+
+export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
