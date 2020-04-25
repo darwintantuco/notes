d79acdddc7d8e78682c4e9e832e6cbcc1c4fc4ab
darwin christopher tantuco <dcrtantuco@gmail.com>
2020-04-25 23:41:15 +0800

    Add simple sidebar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,12 +3,17 @@ import React from 'react'
 import '../styles/base.scss'
 import styles from './Layout.module.scss'
 
+import Sidebar from '../components/Sidebar'
+
 interface LayoutProps {
   children: React.ReactNode
 }
 
 const Layout = ({ children }: LayoutProps): JSX.Element => (
-  <div className={styles.container}>{children}</div>
+  <div className={styles.container}>
+    <Sidebar />
+    <main>{children}</main>
+  </div>
 )
 
 export default Layout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,14 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
-import NoteLink from '../components/NoteLink'
 
-const IndexPage = ({
-  data: {
-    allMarkdownRemark: { edges },
-  },
-}): JSX.Element => {
-  const Posts = edges.map((edge) => (
-    <NoteLink key={edge.node.id} post={edge.node} />
-  ))
-
-  return <Layout>{Posts}</Layout>
+const IndexPage = (): JSX.Element => {
+  return (
+    <Layout>
+      <h1> Notes </h1>
+      <p> Collection of notes I find useful in day to day work </p>
+    </Layout>
+  )
 }
 
 export default IndexPage
-
-export const pageQuery = graphql`
-  query {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          id
-          excerpt(pruneLength: 250)
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            path
-            title
-          }
-        }
-      }
-    }
-  }
-`
