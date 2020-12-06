8874d3cfe1f954d8028c42dca2d5463c8ddc8550
darwintantuco <dcrtantuco@gmail.com>
2020-12-06 09:30:04 +0000

    Deploying to gh-pages from  @ 19c1f005433e8ce31b7dbb94616c4947f7d17ef6 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-086c4d5ab058f8106f87.js"
+    "url": "webpack-runtime-1ea082ce126daf603590.js"
   },
   {
     "url": "styles.9e64bf774856e4b92f02.css"
@@ -39,11 +39,11 @@ self.__precacheManifest = [
     "url": "framework-37b45856124a722f0eb5.js"
   },
   {
-    "url": "app-9b6c5ae8251c41ea3b56.js"
+    "url": "app-f9f449f3fb6e107fd6c4.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5d822d8a427b9e94636b25030a52e33b"
+    "revision": "feded569edea5197abc3cb39d0f5342e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7efb7f699baadb287da0.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "652ec8665ad377fa6282cce09accce35"
+    "revision": "1551fe617d9fbc3c94f77a733f792ab4"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -147,7 +147,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/notes/app-9b6c5ae8251c41ea3b56.js`))) {
+  if (!resources || !(await caches.match(`/notes/app-f9f449f3fb6e107fd6c4.js`))) {
     return await fetch(event.request)
   }
 
