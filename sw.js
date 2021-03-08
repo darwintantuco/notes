398e9f9d77bd02f1528b95f2add82f5f404daa2c
darwintantuco <dcrtantuco@gmail.com>
2021-03-08 09:42:11 +0000

    Deploying to gh-pages from  @ 7ba0b5ad1dd5d482c9c00a41bf1264b346f42108 🚀

--- sw.js
+++ sw.js
@@ -39,11 +39,11 @@ self.__precacheManifest = [
     "url": "framework-37b45856124a722f0eb5.js"
   },
   {
-    "url": "app-1584711d4abdff3c2fdc.js"
+    "url": "app-1f1226eb4de4e4bf027e.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "08b741b199a71e0dd22e60a7a7e73b60"
+    "revision": "4c78fa45e3ef9edf35d320652bc0c16f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7efb7f699baadb287da0.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "098526bf5a160d7b600570b210b667e9"
+    "revision": "ca32ebf481ab53f36725151c6bd4243f"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -147,7 +147,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/notes/app-1584711d4abdff3c2fdc.js`))) {
+  if (!resources || !(await caches.match(`/notes/app-1f1226eb4de4e4bf027e.js`))) {
     return await fetch(event.request)
   }
 
