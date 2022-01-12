beb7bdb8c2d7f4d7075daf7a57186cc22c2145f6
Darwin Tantuco <dcrtantuco@gmail.com>
2022-01-12 21:01:21 +0800

    DocSearch v3

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,29 +1,14 @@
-import React, { useEffect } from 'react'
-import loadjs from 'loadjs'
-import { Input } from 'antd'
+import React from 'react'
+import { DocSearch } from '@docsearch/react'
+import '@docsearch/css'
 
 const SearchInput = (): JSX.Element => {
-  useEffect(() => {
-    loadjs(
-      [
-        'https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css',
-        'https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js',
-      ],
-      () => {
-        docsearch({
-          apiKey: '6bba7de852f1c94bbca06d233879424d',
-          indexName: 'darwintantuco',
-          inputSelector: '#searchInput',
-          debug: true,
-        })
-      }
-    )
-  }, [])
-
   return (
-    <div className={'mb-8'}>
-      <Input placeholder='Search' id='searchInput' />
-    </div>
+    <DocSearch
+      appId='YW9VSASXWM'
+      apiKey='f53866351b69b4ed8215dd64602da9dc'
+      indexName='darwintantuco'
+    />
   )
 }
 
